5283aed6dd301096bfaf5daf0a8bf5a4bb02c752
aaguiler <aguileragarciadev@gmail.com>
2023-02-11 21:33:21 +0100

    Add ex02 (incomplete)

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaguiler < aaguiler@student.42malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/11 21:27:28 by aaguiler          #+#    #+#             */
+/*   Updated: 2023/02/11 21:32:23 by aaguiler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+# include <iterator>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+
+};
+
+#endif
