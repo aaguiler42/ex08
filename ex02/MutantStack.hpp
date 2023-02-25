5da5cdd717c3875c8228cc996bae32bbfbc33f87
archlex <aguileragarciadev@gmail.com>
2023-02-25 15:32:16 +0100

    Add main and finish ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaguiler < aaguiler@student.42malaga.co    +#+  +:+       +#+        */
+/*   By: aaguiler <aaguiler@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 21:27:28 by aaguiler          #+#    #+#             */
-/*   Updated: 2023/02/11 21:32:23 by aaguiler         ###   ########.fr       */
+/*   Updated: 2023/02/25 15:21:36 by aaguiler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,25 @@
 template <typename T>
 class MutantStack : public std::stack<T>
 {
+	public:
+	MutantStack() : std::stack<T>() {}
+	MutantStack(MutantStack const &src) : std::stack<T>(src) {}
+	~MutantStack() {}
+
+	MutantStack &operator=(MutantStack const &src)
+	{
+		if (this != &src)
+			std::stack<T>::operator=(src);
+		return *this;
+	}
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	iterator begin() { return std::stack<T>::c.begin(); }
+	iterator end() { return std::stack<T>::c.end(); }
+	const_iterator begin() const { return std::stack<T>::c.begin(); }
+	const_iterator end() const { return std::stack<T>::c.end(); }
 
 };
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,17 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaguiler < aaguiler@student.42malaga.co    +#+  +:+       +#+        */
+/*   By: aaguiler <aaguiler@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 21:26:59 by aaguiler          #+#    #+#             */
-/*   Updated: 2023/02/11 21:32:48 by aaguiler         ###   ########.fr       */
+/*   Updated: 2023/02/25 15:28:33 by aaguiler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <iostream>
+#include <list>
 
-int main(void)
+int main()
 {
-
-  return 0;
+	{
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	{
+		std::list<int> mstack;
+		mstack.push_back(5);
+		mstack.push_back(17);
+		std::cout << mstack.back() << std::endl;
+		mstack.pop_back();
+		std::cout << mstack.size() << std::endl;
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		//[...]
+		mstack.push_back(0);
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::list<int> s(mstack);
+	}
+	return 0;
 }
